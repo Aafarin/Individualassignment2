b961668ef3ac0f158884de7c8045deeccf6bf0cb
Aafarin <aafrinnadaf78@gmail.com>
2021-12-08 13:00:08 +0530

    initial commit

---
+++ src/individualAssignment/Array.java
@@ -0,0 +1,17 @@
+package individualAssignment;
+
+public class Array {
+
+	public static void main(String[] args) {
+		   int arr[]= {11, 12, 13, 14, 15};   
+	          
+	        System.out.println("Elements of given array: "); 
+	        
+	        
+	        for (int i = 0; i < arr.length; i++)
+	        {   
+	            System.out.print(arr[i] + " ");   
+	        }    
+	}
+
+}
---
+++ src/individualAssignment/Lengthofarray.java
@@ -0,0 +1,17 @@
+package individualAssignment;
+
+public class Lengthofarray {
+
+	public static void main(String[] args) {
+
+		 int arr[] = new int[5];
+		 
+       	 int arr2[] = new int[5];
+       	 
+       	 System.out.print(arr.length + " ");
+       	 
+       	 System.out.print(arr2.length+" ");
+
+	}
+
+}
