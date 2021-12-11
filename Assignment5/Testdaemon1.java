3d04731d182c232c6addfe2f2106d6a05ec58bba
Aafarin <95407055+Aafarin@users.noreply.github.com>
2021-12-11 15:07:43 +0530

    Add files via upload

---
+++ Assignment5/Testdaemon1.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+
+
+public class Testdaemon1 extends Thread 
+{
+
+	public void run()
+	{  
+        System.out.println("Running thread is daemon...");  
+    }  
+	
+public static void main (String[] args) 
+{  
+    Testdaemon1 td= new Testdaemon1();
+    
+    td.start();
+    
+    td.setDaemon(true);  
+    
+	}
+
+}
